dc8abc37673037207c0365ab8cafebcab067afd6
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-11-09 14:56:53 +0000

    finalização de layout - tela checkout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { globalStyles } from "../styles/global"
 import logoImg from "../assets/logo.svg"
-import { Container, Header, ShoppingCart } from "../styles/pages/app"
+import { Container, ContentCard, Header, ImageContainer, ShoppingCart } from "../styles/pages/app"
 import Image from 'next/image'
 import Link from "next/link"
 import { Handbag, X } from "phosphor-react"
@@ -31,9 +31,48 @@ export default function App({ Component, pageProps }) {
         </Header>
 
         <ShoppingCart className={cartItems}>
-          <button type="button" onClick={handleOpenMenu} title="close Cart">
+          <button type="button" id="close" onClick={handleOpenMenu} title="close Cart">
             <X size={32} /> 
           </button>
+
+          <ContentCard>
+            <div>  
+              <h3>Sacola de compras</h3>
+
+              <div className="itensCart">
+                <div className="item">
+                  <ImageContainer>
+
+                  </ImageContainer>
+
+                  <div className="details">
+                    <p>Camiseta Beyond The Limits</p>
+                    <strong>R$ 79,90</strong>
+                    <button type="button">Remover</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="checkoutCart">
+              <div className="purchaseDetails">
+                <span>
+                  <p>Quantidade</p>
+                  <p>1 item</p>
+                </span>
+
+                <span>
+                  <strong>Valor total</strong>
+                  <strong id="total">R$ 79,90</strong>
+                </span>
+              </div>
+
+              <button type="button" title="Finalizar Compra">
+                Finalizar compra
+              </button>
+              
+            </div>
+          </ContentCard>
         </ShoppingCart>
 
         <Component {...pageProps} />
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -1,3 +1,4 @@
+import { Divide } from "phosphor-react";
 import { styled } from "..";
 
 export const Container = styled('div', {
@@ -48,15 +49,125 @@ export const ShoppingCart = styled('div', {
     display: 'initial',
   },
 
-  button: {
+  '#close': {
     border: 'none',
     background: 'none',
     color: '$gray100',
     cursor: 'pointer',
+    position: 'absolute',
+    right: '3rem',
 
     '&:hover': {
       color: '#d42020',
       transition: 'all 0.2s'
     }
+  },
+
+  '.checkoutCart': {
+    button: {
+      marginTop: '3.4rem',
+      padding: '1.25rem',
+      width: '100%',
+      borderRadius: 4,
+      border: 'none',
+      cursor: 'pointer',
+      background: '$green500',
+      color: '$gray100',
+      fontWeight: 'bold',
+      fontSize: '$md',
+
+      '&:hover': {
+        background: '$green300',
+        transition: 'all 0.2s'
+      }
+    }
   }
 })
+
+export const ContentCard = styled('div', {
+  marginTop: '4.5rem',
+  height: '82vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+
+  '.itensCart': {
+    marginTop: '2rem'
+  },
+
+  '.item': {
+    display: 'flex',
+    gap: '1.25rem',
+
+    strong: {
+      color: '$green300'
+    },
+
+    p: {
+      color: '$gray300'
+    },
+
+    button: {
+      width: '30%',
+      // margin: '0 auto',
+      color: '$green500',
+      border: 'none',
+      background: 'none',
+      fontWeight: 'bold',
+      cursor: 'pointer',
+
+      '&:hover': {
+        color: '#d42020',
+        transition: 'all 0.2s'
+      }
+    }
+  },
+
+  '.details': {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+
+  '.purchaseDetails': {
+    display: 'flex',
+    gap: '0.5rem',
+    flexDirection: 'column',
+
+    span: {
+      display: 'flex',
+      justifyContent: 'space-between',
+
+      p: {
+        color: '$gray300',
+        fontSize: 16
+      },
+
+      strong: {
+        color: '$gray100'
+      },
+
+      '#total': {
+        fontSize: '$lg'
+      }
+    }
+  },
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 101,
+  height: 93,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+
+  img: {
+    objectFit: 'cover',
+  }
+});
